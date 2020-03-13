5cfe4117d3a7a099be85cf5430ca2b5d6202e034
adawyj97 <49011074+adawyj97@users.noreply.github.com>
2020-03-13 16:33:16 -0400

    created some functions

--- assignment/midtermFunctions.js
+++ assignment/midtermFunctions.js
@@ -1,3 +1,30 @@
 /* ================================
 Week 6 Assignment: Midterm Functions + Signatures
 ================================ */
+
+// Get specimen data
+var specimenData = ???
+
+// Get state boundary data
+var stateData = ???
+
+// Join the specimen data to the state data
+var joinData = function(specimenData, stateData) {};
+
+// Find the species with the most specimens collected in the selected state
+var maxSpecies = function(joinData, selectedState) {};
+
+// Find the collector who have collected the most specimens in the selected state
+var maxCollector = function(joinData, selectedState) {};
+
+// Find the total number of specimens collected in the selected state
+var totalSpecimens = function(joinData, selectedState) {};
+
+// Filter the specimen data with the collector of the specimens
+var filterCollector = function(specimenData, userinputCollector) {};
+
+// Filter the specimen data with the species of the specimens
+var filterSpecies = function(specimenData, userinputSpecies) {};
+
+// Filter the specimen data with the state where the specimens were collected
+var filterState = function(specimenData, userinputState) {};
--- assignment/slideModel.js
+++ assignment/slideModel.js
@@ -11,7 +11,32 @@ var slideExample = {
   filter: function(geojsonFeature) { return true }
 };
 
+var slideExample1 = {
+  slideNumber: 1,
+  title: "Species of the specimens",
+  style: function(data) {
+    // Color the points according to the species of the specimens
+  }
+};
+
+var slideExample2 = {
+  slideNumber: 2,
+  title: "Collectors of the specimens",
+  style: function(data) {
+    // Color the points according to the collectors of the specimens
+  }
+};
+
+var slideExample3 = {
+  slideNumber: 3,
+  title: "Temporal distribution of the collection",
+  style: function(data) {
+    // According to the time the specimens were collected, map the points across time
+  }
+};
+
+
 /** Here's the simplest implementation I could come up with for
  * representing a deck of slides (nothing exotic is necessary!)
  */
-var slideDeck = [slideExample1, slideExample2, slideExample3]
+var slideDeck = [slideExample1, slideExample2, slideExample3];
