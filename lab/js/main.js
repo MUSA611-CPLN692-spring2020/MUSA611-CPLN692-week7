272af5049f506fe22230890a6f538bd26272cdc5
Leo <48158850+leonardoharth@users.noreply.github.com>
2020-03-19 14:56:32 -0400

    update wireframe

--- lab/js/main.js
+++ lab/js/main.js
@@ -216,22 +216,19 @@ function getColor(d) {
            d === "FRI" '#e6f598' :
                       '#FFEDA0';
 }
-*/
-
-
 
 var legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = ["SUN", "MON", "TUE", "WED", "THU", "FRI"],
+        grades = ["MON", "TUE", "WED", "THU", "FRI"],
         labels = [];
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            '<i style="background:' + getColor(grades[i]) + '"></i> ' +
             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
@@ -239,3 +236,4 @@ legend.onAdd = function (map) {
 };
 
 legend.addTo(map);
+*/
