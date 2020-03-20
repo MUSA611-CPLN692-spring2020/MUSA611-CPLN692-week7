f9973ca639fcc2c3a35052f570db95c70d829188
HanyongXu <xuhanyon@upenn.edu>
2020-03-19 20:31:48 -0400

    lab + assignment week 7

--- assignment/midtermFunctions.js
+++ assignment/midtermFunctions.js
@@ -1,3 +1,32 @@
 /* ================================
 Week 6 Assignment: Midterm Functions + Signatures
 ================================ */
+var dataset = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-18-2020.csv";
+var census = "link here"
+var featureGroup;
+
+//request data from url and parse csv into arrays
+var parseCSV = function(dataset) { }
+
+//join the US data and the US census
+var joinFunction = function(cases, census){ }
+
+//define colors or size of the individual marker with respect to covid 19 cases
+var myStyle = function(feature) { }
+
+//show results
+var showResults = function() { }
+
+//close results and return to original state
+var closeResults = function() { }
+
+//change side bar information with respect to each country
+var eachFeatureFunction = function(layer) {
+  layer.on('click', function(event){
+
+  })
+}
+
+//run the analysis by start the request of the dataset
+$(document).ready(function() {
+}
