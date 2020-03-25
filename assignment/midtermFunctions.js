6c56ab8c1a194e327f546cb8eecb57312b0aab4b
njxinran95 <njxinran@hotmail.com>
2020-03-25 19:20:57 -0400

    UPDATE

--- assignment/midtermFunctions.js
+++ assignment/midtermFunctions.js
@@ -1,3 +1,11 @@
 /* ================================
 Week 6 Assignment: Midterm Functions + Signatures
 ================================ */
+var dataset ="https://raw.githubusercontent.com/MUSA611-CPLN692-spring2020/datasets/master/geojson/housingprice_Beijing.geojson";
+
+$(document).ready(function() {
+$.ajax(dataset).done(function(data) {
+  var parsedData = JSON.parse(data);
+  console.log(parsedData);
+});
+});
